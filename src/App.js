cf87c455f931262713318511821985b7591ceb4c
rashid sahil <rashidsahil1@gmail.com>
2022-10-18 12:05:53 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  let shows;
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(
+      "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+    ).then((result) => {
+      result.json().then((resp) => {
+        let entries = Object.entries(resp);
+        let dat = entries[1];
+        let newquery = Object.entries(dat[1]);
+        setData(newquery);
+      });
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="centralised">
+      <h1>Table for assignment</h1>
+      <table className="back">
+        <tbody>
+          <tr>
+            <th className="hid">Id</th>
+            <th>DateTime</th>
+            <th>Open</th>
+            <th>High</th>
+            <th>Low</th>
+            <th>Close</th>
+            <th>Volume</th>
+          </tr>
+
+          {data?.map((item) => (
+            <tr key={item[0]}>
+              <td className="hid">
+                {(shows = Object.entries(item[1]))}
+              </td>
+
+              <td>{item[0]}</td>
+              <td> {(shows[0])[1]}</td>
+              <td>{(shows[1])[1]}</td>
+              <td>{(shows[2])[1]}</td>
+              <td>{(shows[3])[1]}</td>
+              <td>{(shows[4])[1]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </>
   );
 }
 
